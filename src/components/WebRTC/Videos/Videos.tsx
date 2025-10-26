5fa03e5a45305fae4a8ce3d05400ac6dfa6dadd7
Rick Smith <ricksmith26@hotmail.com>
2025-10-26 15:59:28 +0000

    reworkign styling

--- src/components/WebRTC/Videos/Videos.tsx
+++ src/components/WebRTC/Videos/Videos.tsx
@@ -12,24 +12,14 @@ interface VideosProps {
 export const Videos = ({ areVisible, localVideoRef, remoteVideoRef }: VideosProps) => {
     return (
         <>
-            {<div style={{ visibility: areVisible ? 'visible' : 'hidden' }}>
-                <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%' }}>
-                    <video ref={remoteVideoRef} autoPlay playsInline
-                        style={{
-                            width: 'max-content',
-                            height: 'max-content',
-                            maxHeight: '100%',
-                            maxWidth: '100%',
-                            objectFit: 'contain',
-                            backgroundColor: '#000',
-                            zIndex: -1
-                        }}></video>
+            <div className={`videosContainer ${areVisible ? 'visible' : ''}`}>
+                <div className="remoteVideoContainer">
+                    <video ref={remoteVideoRef} autoPlay playsInline className="remoteVideo"></video>
                 </div>
                 <div className="localVideo">
-                    <video ref={localVideoRef} autoPlay playsInline muted style={{ height: '150px', zIndex: 100 }}></video>
+                    <video ref={localVideoRef} autoPlay playsInline muted></video>
                 </div>
-            </div>}
+            </div>
         </>
-
     )
 }
